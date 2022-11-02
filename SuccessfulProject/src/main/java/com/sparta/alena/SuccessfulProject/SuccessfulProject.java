dbd4fdce2351f3298ed91eb5a32ab10ad4c569ee
Alena Krasheninnikova <alenakrash@mail.ru>
2022-11-02 18:08:33 +0300

    First no-sense commit

---
+++ SuccessfulProject/src/main/java/com/sparta/alena/SuccessfulProject/SuccessfulProject.java
@@ -0,0 +1,53 @@
+package com.sparta.alena.SuccessfulProject;
+
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class SuccessfulProject {
+    public static void main(String[] args) {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.postcodes.io/postcodes/EC2Y5AS"))
+                .setHeader("Content-type", "application/json")
+                .build();
+        HttpResponse<String> httpResponse = null;
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(httpResponse);
+        System.out.println("URI: " + httpResponse.uri());
+        System.out.println("Headers: " + httpResponse.headers());
+        System.out.println(("Status code: " + httpResponse.statusCode()));
+        System.out.println();
+        Map<String, List<String>> headers = httpResponse.headers().map();
+        for (var item : headers.entrySet()) {
+            System.out.print(item.getKey() + " ");
+            System.out.println(item.getValue());
+        }
+
+        System.out.println("Interrogating the response body using simple-json");
+        JSONParser jsonParser = new JSONParser();
+        try {
+            JSONObject jsonObject = (JSONObject)jsonParser.parse(httpResponse.body());
+            System.out.println(jsonObject.get("status"));
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ SuccessfulProject/src/test/java/com/sparta/alena/SuccessfulProjectTesting/SuccessfulProject_Testing.java
@@ -0,0 +1,85 @@
+package com.sparta.alena.SuccessfulProjectTesting;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class SuccessfulProject_Testing {
+
+    private static HttpResponse<String> httpResponse = null;
+    private static JSONObject jsonObject = null;
+
+    @BeforeAll
+    public static void oneTimeSetUp() {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.postcodes.io/postcodes/EC2Y5AS"))
+                .setHeader("Content-type", "application/json")
+                .build();
+
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JSONParser jsonParser = new JSONParser();
+        try {
+            jsonObject = (JSONObject)jsonParser.parse(httpResponse.body());
+//            System.out.println(jsonObject.get("status"));
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("URI Path")
+    public void testUriPath(){
+        Assertions.assertEquals("/postcodes/EC2Y5AS", httpResponse.uri().getPath());
+    }
+
+    @Test
+    @DisplayName("URI")
+    public void testUri(){
+        Assertions.assertEquals("https://api.postcodes.io/postcodes/EC2Y5AS", httpResponse.uri().toString());
+    }
+
+    @Test
+    @DisplayName("Longitude")
+    public void testLongitude(){
+        var result = (JSONObject)jsonObject.get("result");
+        Assertions.assertEquals(-0.09392, result.get("longitude"));
+    }
+
+    @Test
+    @DisplayName("Status Code")
+    public void testStatusCode(){
+        Assertions.assertEquals(200, httpResponse.statusCode());
+    }
+
+    @Test
+    @DisplayName("Header Server is cloudflare")
+    public void testHeaderServer(){
+        Assertions.assertTrue(httpResponse.headers().map().get("Server").contains("cloudflare"));
+    }
+
+    @Test
+    @DisplayName("Status is 200")
+    public void testStatus(){
+        Assertions.assertEquals(200L, jsonObject.get("status"));
+    }
+
+}
